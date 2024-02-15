8d1136c9286fc07b1af9a3a5d2f2e2907bfe1504
Botk <111537394+b0tk@users.noreply.github.com>
2024-02-15 01:02:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function startGame() {
     }, 100);
 
     document.addEventListener('keydown', handleKeyDown);
-    document.getElementById('police-lights').style.animation = 'none';
+    // document.getElementById('police-lights').style.animation = 'none';
 }
 
 function generateLetters() {
